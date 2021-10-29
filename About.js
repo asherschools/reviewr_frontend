81b668ac2217f14ad8e3b1d08b4e293204e89c0d
asherschools <47398007+asherschools@users.noreply.github.com>
2021-10-29 14:01:59 -0400

    Add files via upload

---
+++ About.js
@@ -0,0 +1,19 @@
+import './About.css';
+
+function About() {
+  return (
+    <div className="About">
+      <header className="App-header">
+        <title>
+          About
+        </title>
+        <p>
+          this is the about page.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default About;
---
+++ Home.js
@@ -0,0 +1,19 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <title>
+          Home
+        </title>
+        <p>
+          this is the home page.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Home;
